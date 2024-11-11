60b5fb4a4091c7fe845da7bae4ed91c077e44877
wellupweb <wellupweb@gmail.com>
2024-11-11 15:20:51 +0600

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div className="text-3xl text-center py-10 text-red-500">
+        Hello world!
+      </div>
+    ),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
